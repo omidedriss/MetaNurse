95d50458bced7eba94b1d25473d8870774f35914
Abbas <absgorji@gmail.com>
2022-09-22 01:39:15 +0330

    V:1.0

--- app/src/main/java/com/example/metacogroup/login.java
+++ app/src/main/java/com/example/metacogroup/login.java
@@ -2,13 +2,73 @@ package com.example.metacogroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class login extends AppCompatActivity {
+EditText pass,codePersonally;
+TextView singUp,forgetPass;
+Button buttonLogin;
+private String name,family,password;
+private String codmeli , codPersonally , number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        entry();
+        inPut();
+        //checker();
     }
+    //هنگام اجرا برنامه ادیت  تکس ها و ... مچ میشن
+    public void entry (){
+        pass = findViewById(R.id.passwordlogin);
+        codePersonally = findViewById(R.id.codemelipersoneli);
+        singUp = findViewById(R.id.donthaveacount);
+        forgetPass = findViewById(R.id.forgetpassword);
+        buttonLogin = findViewById(R.id.login_btn);
+        singUp.setOnClickListener(v -> {
+            Intent i = new Intent(getApplicationContext(),signup.class);
+            startActivity(i);
+            finish();
+                });
+        forgetPass.setOnClickListener(v -> {
+          //  Intent i = new Intent(getApplicationContext(),forget.class);
+           // startActivity(i);
+        });
+    }
+    //دریافت اطلاعات از sharepreferences
+    public void inPut (){
+        SharedPreferences shareName = getSharedPreferences("namee", Context.MODE_PRIVATE);
+        name = shareName.getString("namee","noName");
+       // SharedPreferences shareFamily = getSharedPreferences("family", Context.MODE_PRIVATE);
+        family = shareName.getString("familyy","noFamily");
+      //  SharedPreferences sharePassword = getSharedPreferences("password", Context.MODE_PRIVATE);
+        password = shareName.getString("passwordd","noPass");
+       // SharedPreferences shareCodmeli = getSharedPreferences("codmeli", Context.MODE_PRIVATE);
+        codmeli = shareName.getString("codmeli","0");
+       // SharedPreferences shareCodePersonally = getSharedPreferences("codepersonally", Context.MODE_PRIVATE);
+        codPersonally = shareName.getString("codepersonally","0");
+       // SharedPreferences shareNumber = getSharedPreferences("codepersonally", Context.MODE_PRIVATE);
+        number = shareName .getString("number","0");
+    }
+/*    public void checker (){
+        if(codmeli !=0 && codPersonally != 0){
+            codePersonally.setText(codPersonally);
+        }else if(codPersonally == 0 && codmeli !=0 ){
+            codePersonally.setText(codmeli);
+        }
+        if(password.startsWith("noPass")){
+            Toast.makeText(getApplicationContext(),password,Toast.LENGTH_LONG).show();
+        }else {
+            pass.setText(password);
+        }
+    }*/
+
 }
--- app/src/main/java/com/example/metacogroup/signup.java
+++ app/src/main/java/com/example/metacogroup/signup.java
@@ -2,20 +2,92 @@ package com.example.metacogroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import javax.xml.validation.Validator;
 
 public class signup extends AppCompatActivity {
-TextView signup;
+TextView vorud,backLogin;
+Button btSingUp;
+EditText password,confirmPassword,name,family,codMeli,phoneNumber,codePersonally;
+RadioButton doctor,nurse,pationt;
+int codeMeli1,codePersonaly1,number1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-        signup=findViewById(R.id.signup);
+        entry();
         Typeface font = Typeface.createFromAsset(getAssets(), "saeidian_font/BNazanin.ttf");
+        vorud.setTypeface(font);
+    }
+    public void entry (){
+        vorud=findViewById(R.id.signup);
+        password = findViewById(R.id.password);
+        confirmPassword = findViewById(R.id.confirmpassword);
+        codePersonally = findViewById(R.id.codepersonally);
+        name = findViewById(R.id.name);
+        family = findViewById(R.id.family);
+        codMeli= findViewById(R.id.codemeli);
+        phoneNumber = findViewById(R.id.phone);
+        doctor = findViewById(R.id.doctor);
+        nurse = findViewById(R.id.nurse);
+        pationt = findViewById(R.id.patient);
+        backLogin = findViewById(R.id.haveaccount);
+        btSingUp = findViewById(R.id.signup_btn);
+        pationt.setChecked(true);
+        btSingUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        signup.setTypeface(font);
+ /*           if(!pationt.isChecked()){
+
+            }*/
+                if(password.getText().toString().equalsIgnoreCase(confirmPassword.getText().toString())
+                        &&phoneNumber.getText().toString().startsWith("09")&&phoneNumber.getText().toString().length()==11&&
+                        codMeli.getText().toString().length()==10){
+                    Intent i = new Intent(getApplicationContext(),getcode.class);
+                    putExtra();
+                    startActivity(i);
+            }
+
+               else {
+                    Toast.makeText(getApplicationContext(),"اطالاعات وارد شده نادرست است",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+
+
+
+        backLogin.setOnClickListener(v -> {
+            Intent i = new Intent(getApplicationContext(),login.class);
+            startActivity(i);
+            finish();
+        });
+    }
+    public void putExtra(){
+       // codeMeli1 = Integer.getInteger(codMeli.getText().toString());
+       // codePersonaly1 = Integer.getInteger(codePersonally.getText().toString());
+       // number1 = Integer.getInteger(phoneNumber.getText().toString());
+            SharedPreferences shareName = getSharedPreferences("namee", Context.MODE_PRIVATE);
+             SharedPreferences.Editor editor = shareName.edit();
+                  editor.putString("namee",String.valueOf(name.getText()));
+                  editor.putString("familyy",String.valueOf(family.getText()));
+                  editor.putString("number",String.valueOf(phoneNumber.getText()));
+                  editor.putString("passwordd",String.valueOf(password.getText()));
+                  editor.putString("codmeli",codMeli.getText().toString());
+                  editor.putString("codepersonally",codePersonally.getText().toString());
+                  editor.commit();
     }
 }
