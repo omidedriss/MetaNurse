ebb140a705ad462ad63034000f6e099c1892e488
Abbas <absgorji@gmail.com>
2022-09-22 03:07:49 +0330

    V:1.2

--- app/src/main/java/com/example/metacogroup/menu.java
+++ app/src/main/java/com/example/metacogroup/menu.java
@@ -1,8 +1,16 @@
 package com.example.metacogroup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class menu extends AppCompatActivity {
 
@@ -10,5 +18,22 @@ public class menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.exit_menu) {
+            Toast.makeText(getApplicationContext(), "صفحه خروج اجباری :)", Toast.LENGTH_LONG).show();
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
