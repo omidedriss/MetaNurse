d30de29a7b9891f0a80b5cce22c119a196bad02b
Hatami <alireza.hatami905@gmail.com>
2022-09-22 06:56:12 +1000

    layout background getcode2

--- app/src/main/java/com/example/metacogroup/getcode.java
+++ app/src/main/java/com/example/metacogroup/getcode.java
@@ -1,7 +1,9 @@
 package com.example.metacogroup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 
 public class getcode extends AppCompatActivity {
@@ -10,5 +12,11 @@ public class getcode extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_getcode);
+        ConstraintLayout constraintLayout =findViewById(R.id.color_ss);
+
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2500);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
     }
 }
