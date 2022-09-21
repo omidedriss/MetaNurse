6f604bb4ae950a9d9e543eb0436974e1a35a0e8b
Hatami <alireza.hatami905@gmail.com>
2022-09-22 06:01:22 +1000

    videoView

--- app/src/main/java/com/example/metacogroup/MainActivity.java
+++ app/src/main/java/com/example/metacogroup/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.metacogroup;
-
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.VideoView;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    VideoView videoView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        videoView =findViewById(R.id.videoView2);
+        videoView.setVideoPath("android.resource://"+ getPackageName()+"/"+R.raw.metaco11);
+        videoView.start();
+        videoView.setOnCompletionListener(mp -> {
+            Intent i = new Intent(MainActivity.this,login.class);
+            startActivity(i);
+            finish();
+        });
+
+
+
+
+
     }
 }
