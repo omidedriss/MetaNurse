18851123b3da33ed0045fe0e7ad3e23bc978ba1d
Abbas <absgorji@gmail.com>
2022-09-22 12:00:22 +0330

    V:1.3

--- app/src/main/java/com/example/metacogroup/editProfile.java
+++ app/src/main/java/com/example/metacogroup/editProfile.java
@@ -3,12 +3,42 @@ package com.example.metacogroup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class editProfile extends AppCompatActivity {
-
+public class editProfile extends AppCompatActivity implements View.OnClickListener {
+EditText pName,pFamily,pPhone;
+ImageView img;
+TextView myCode;
+Button ok;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+        entry();
+    }
+    public void entry(){
+        pName = findViewById(R.id.editName);
+        pFamily = findViewById(R.id.editFamily);
+        pPhone = findViewById(R.id.editPhone);
+        myCode = findViewById(R.id.myCodPersonally);
+        img = findViewById(R.id.myPicProfile);
+        ok = findViewById(R.id.ok);
+        ok.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == ok.getId()){
+            pName.setText("");
+            pFamily.setText("");
+            pPhone.setText("");
+            Toast.makeText(getApplicationContext(),"ثبت شد",Toast.LENGTH_LONG).show();
+            finish();
+        }
     }
 }
